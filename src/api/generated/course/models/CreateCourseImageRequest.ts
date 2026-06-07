8341c66ad0a1d634cd4e6821e07feb11f9dcda2f
iamkvnn <23110119@studnet.hcmute.edu.vn>
2026-06-07 18:35:16 +0700

    chore: delete old api

--- src/api/generated/course/models/CreateCourseImageRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { ImageDto } from './ImageDto';
-export type CreateCourseImageRequest = {
-    images: Array<ImageDto>;
-};
-
